35447d5003e77dc82754cd3315634e902400c5a7
saito hiraku <hiraku.saito@thedott.io>
2024-02-26 16:22:12 +0900

    issue4 レイアウト等、修正

--- intern-book-app/src/app/types/mock-book.ts
+++ intern-book-app/src/app/types/mock-book.ts
@@ -3,6 +3,5 @@ import { Book } from './book';
 export const bookList: Book[] = [
   { name: '告白', detail: '湊かなえのイヤミスデビュー作', evaluation: 100},
   { name: 'りぽぐら', detail: '西尾維新がすごいなって作品', evaluation: 70},
-  { name: '近畿地方のある場所について', detail: '最近のホラー小説', evaluation: 90},
-
+  { name: '近畿地方のある場所について', detail: '最近のホラー小説', evaluation: 90}
 ];
