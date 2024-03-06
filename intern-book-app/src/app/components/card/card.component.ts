4016a7a1371d78f88a420d9110a0069bc4285b7c
saito hiraku <hiraku.saito@thedott.io>
2024-03-07 04:24:01 +0900

    issue9 書籍を削除するときにダイアログで確認

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input} from '@angular/core';
 import { bookList } from '../../types/mock-book';
 import { Book } from '../../types/book';
 import { Output, EventEmitter } from '@angular/core';
+import { MatDialog,MatDialogRef } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
@@ -10,15 +12,38 @@ import { Output, EventEmitter } from '@angular/core';
 })
 
 export class CardComponent {
-  @Input() bookcard:Book = {
+  @Input() bookcard: Book = {
     name: "test",
-    detail:"test",
+    detail: "test",
     evaluation: 0
   };
   @Output() deleteBookEvent = new EventEmitter<Book>();
 
-  deleteBook() {
-    this.deleteBookEvent.emit(this.bookcard);
+  constructor(public dialog: MatDialog) {}
+
+  deleteBook(): void {
+    const dialogRef = this.dialog.open(Dialog, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteBookEvent.emit(this.bookcard);
+      }
+    });
   }
 }
 
+  @Component({
+    selector: 'app-dialog',
+    templateUrl: './dialog.html'
+  })
+
+  export class Dialog {
+    constructor(public dialogRef: MatDialogRef<Dialog>) {}
+    cancel(): void {
+      this.dialogRef.close();
+    }
+  }
+  
+  
